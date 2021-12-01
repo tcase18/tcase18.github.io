27b5506de5a6dcd8ad876ff7267e29c9e070bbb2
tcase18 <93289181+tcase18@users.noreply.github.com>
2021-11-30 22:29:14 -0700

    bootstrap

---
+++ 01Fitness-BootstrapStudent/01Fitness-BootstrapStudent/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Timothy Case
+    File Name: script.js
+    Date: 11/30/2021
+*/
+
+//JQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero').height($(window).height());
+});
